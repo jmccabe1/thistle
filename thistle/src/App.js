6de41997b8cbaa9d6df686456bf2d90b31cce2ef
JohnMcCabe <fredthefurrypotato@hotmail.com>
2021-01-07 16:24:02 -0500

    React header (close enough)

--- thistle/src/App.js
+++ thistle/src/App.js
@@ -5,17 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello World -- Header</h1>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+
         </a>
       </header>
     </div>
