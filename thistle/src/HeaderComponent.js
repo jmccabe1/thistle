3c5f74e67b500d5cbc63778e1a6fc417689d907a
JohnMcCabe <fredthefurrypotato@hotmail.com>
2021-01-07 17:15:41 -0500

    React header 2

---
+++ thistle/src/HeaderComponent.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World -- Header</h1>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
