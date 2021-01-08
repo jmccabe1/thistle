5314bb76f6e236d927332d2fb412e2df28f64c94
JohnMcCabe <fredthefurrypotato@hotmail.com>
2021-01-08 18:01:00 -0500

    first css

--- thistle/src/HeaderComponent.js
+++ thistle/src/HeaderComponent.js
@@ -6,14 +6,37 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World -- Header</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-        </a>
+        <div className="HeaderLinks">
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >Home</a><a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >About Us</a>
+            <a
+            className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+            >Products</a>
+            <a
+            className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+            >Tips</a>
+            <a
+            className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+            >Contact Us</a>
+        </div>
       </header>
     </div>
   );
