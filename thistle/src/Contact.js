7f5771dc5549cd0f3e40894a76efb905890f1d41
JohnMcCabe <fredthefurrypotato@hotmail.com>
2021-01-25 15:39:49 -0500

    basic changes as requested

--- thistle/src/Contact.js
+++ thistle/src/Contact.js
@@ -10,20 +10,19 @@ const Contact = () => {
     return (
     <div>
     <div className='TipTitle'>
-        <h2><a href="mailto:thistlecreekhoneycompany@gmail.com">Reach out to us via email</a></h2>
         </div>
         <div className='TipText'>
-        <p>We don’t sell our products online, but you can contact us at the link above or find out products at one of the local retailers we’ve
-        partnered with:</p>
+        <p>We don’t sell our products online, but you can <a href="mailto:thistlecreekhoneycompany@gmail.com"> contact us</a> directly
+        via email or find out products at one of the local retailers we’ve partnered with:</p>
         </div>
         <div className="partners">
             <div className="partnerLink1">
-                <a href="https://www.unwinedcandles.com">
+                <a href="https://www.unwinedcandles.com" target="_blank">
                     <img src="unwined.png" alt="Unwined Candles"></img>
                 </a>
             </div>
             <div className="partnerLink2">
-                <a href="https://www.facebook.com/Its-Neighbor-Made-104643704440968/">
+                <a href="https://www.facebook.com/Its-Neighbor-Made-104643704440968/" target="_blank">
                     <img src="https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-1/p320x320/100514833_154888336083171_9462517327724544_n.jpg?_nc_cat=100&ccb=2&_nc_sid=dbb9e7&_nc_ohc=oOaP1TpsexUAX9iG0qH&_nc_ht=scontent-iad3-1.xx&tp=6&oh=c0ce2121b0cf1bd7331263b3b11741ce&oe=602E616C" alt="It's Neighbor Made"></img>
                 </a>
             </div>
