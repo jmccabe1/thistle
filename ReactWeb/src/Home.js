586d494fbbd14ca91cbf29693aa6fc2e646f227a
JohnMcCabe <fredthefurrypotato@hotmail.com>
2021-05-18 16:23:16 -0400

    swapped home page image

--- ReactWeb/src/Home.js
+++ ReactWeb/src/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
     return (
         <div className="HomePage">
             <div className="HomeImage">
-                <img src="honeybottlesunset5.png"></img>
+                <img src="websitetopimage.png"></img>
 
             </div>
 
